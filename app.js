5955aace2cf15831eb2f197503237ba8e35a62e9
Margarita Shabanova <marg.alexandrova2018@yandex.ru>
2022-09-14 13:30:26 +0300

    feat: add controllers and routes (GET, POST, DELETE) for cards

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routerUsers = require('./routes/users');
+const routerCards = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/users', routerUsers);
+app.use('/cards', routerCards);
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6320e1d6ceab5b2b307c081a',
+  };
+
+  next();
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
