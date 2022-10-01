98e1e32d22065fa997bcecd3be58c017af83674e
Margarita Shabanova <marg.alexandrova2018@yandex.ru>
2022-10-01 18:17:17 +0300

    fix: edit after tests

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri().regex(pattern).required(),
+    avatar: Joi.string().uri().regex(pattern),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
