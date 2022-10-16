e671154c8da1bad5e8518cdaba21cf9a778269ec
Margarita Shabanova <marg.alexandrova2018@yandex.ru>
2022-10-16 18:14:18 +0300

    feat: edit login and getCards

--- controllers/users.js
+++ controllers/users.js
@@ -56,6 +56,8 @@ const login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
+          sameSite: 'none',
+          secure: true,
         })
         .send({
           _id: user._id,
